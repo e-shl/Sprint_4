d014aefdd7b8b81bb5484b0f956e422670354f93
e-shl <e.shlyapkin99@gmail.com>
2024-10-21 19:09:54 +0500

    clean,fixture,boundary

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # qa_python
 # проверяем, что добавилось именно две книги
-tests.py::TestBooksCollector::test_add_new_book_add_two_books PASSED     [ 11%]
+tests.py::TestBooksCollector::test_add_new_book_add_two_books PASSED     [  7%]
+# проверяем, что Название книги может содержать максимум 40 символов
+tests.py::TestBooksCollector::test_add_new_book_boundary_name[A] PASSED  [ 14%]
+tests.py::TestBooksCollector::test_add_new_book_boundary_name[AA] PASSED [ 21%]
+tests.py::TestBooksCollector::test_add_new_book_boundary_name[20] PASSED [ 28%]
+tests.py::TestBooksCollector::test_add_new_book_boundary_name[39] PASSED [ 35%]
+tests.py::TestBooksCollector::test_add_new_book_boundary_name[40] PASSED [ 42%]
 # проверяем, что у книги установлен жанр
-tests.py::TestBooksCollector::test_set_book_genre_book_set_genre PASSED  [ 22%]
+tests.py::TestBooksCollector::test_set_book_genre_book_set_genre PASSED  [ 50%]
 # проверяем, что жанр книги получен верно
-tests.py::TestBooksCollector::test_get_book_genre_get_correct_genre PASSED [ 33%]
+tests.py::TestBooksCollector::test_get_book_genre_get_correct_genre PASSED [ 57%]
 # проверяем, что по тестовому жанру получен список из одной тестовой книги
-tests.py::TestBooksCollector::test_get_books_with_specific_genre_correct_list_book PASSED [ 44%]
+tests.py::TestBooksCollector::test_get_books_with_specific_genre_correct_list_book PASSED [ 64%]
 # проверяем, что получаемый словарь books_genre такойже как тестовый
-tests.py::TestBooksCollector::test_get_books_genre_correct_books_genre PASSED [ 55%]
+tests.py::TestBooksCollector::test_get_books_genre_correct_books_genre PASSED [ 71%]
 # проверяем, что что тестовой книги с возрастным рейтингом отсутствует в списке книг для детей
-tests.py::TestBooksCollector::test_get_books_for_children_not_age_rating PASSED [ 66%]
+tests.py::TestBooksCollector::test_get_books_for_children_not_age_rating PASSED [ 78%]
 # проверяем, что список избранных содержит тестовую книгу
-tests.py::TestBooksCollector::test_add_book_in_favorites_book_in_favorites PASSED [ 77%]
+tests.py::TestBooksCollector::test_add_book_in_favorites_book_in_favorites PASSED [ 85%]
 # проверяем, что список избранного пуст
-tests.py::TestBooksCollector::test_delete_book_from_favorites_favorites_empty PASSED [ 88%]
+tests.py::TestBooksCollector::test_delete_book_from_favorites_favorites_empty PASSED [ 92%]
 # проверяем, что получаемый словарь favorites такойже как тестовый
 tests.py::TestBooksCollector::test_get_list_of_favorites_books_correct_favorites PASSED [100%]
--- tests.py
+++ tests.py
@@ -5,148 +5,100 @@ from main import BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
-    def test_add_new_book_add_two_books(self):
-        collector = BooksCollector()
-
+    def test_add_new_book_add_two_books(self,collector):
         # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
         # проверяем, что добавилось именно две книги
         assert len(collector.books_genre) == 2
 
-    def test_set_book_genre_book_set_genre(self):
-        collector = BooksCollector()
+    @pytest.mark.parametrize('name', ["A", "AA", "20 тоже может здесь!", "39 символов обязательно должно быть тут", "40 символов обязательно должно быть тута"])
+    def test_add_new_book_boundary_name(self,collector,name):
+        # добавляем книги
+        collector.add_new_book(name)
+        # проверяем, что Название книги может содержать максимум 40 символов
+        assert collector.books_genre == {name : ""}
 
+    def test_set_book_genre_book_set_genre(self,collector):
         # создаём переменные теста
         name = 'Гордость и предубеждение и зомби'
         genre = 'Ужасы'
-
         # добавляем книгу
         collector.add_new_book(name)
-
         # устанавливаем жанр добавленной книге
         collector.set_book_genre(name, genre)
-
         # проверяем, что у книги установлен жанр
         assert collector.books_genre[name] == genre
 
-    def test_get_book_genre_get_correct_genre(self):
-        collector = BooksCollector()
-
+    def test_get_book_genre_get_correct_genre(self,collector):
         # создаём переменные теста
         name = 'Гордость и предубеждение и зомби'
         genre = 'Ужасы'
-
         # добавляем книгу
         collector.add_new_book(name)
-
         # устанавливаем жанр добавленной книге
         collector.set_book_genre(name, genre)
-
         # проверяем, что жанр книги получен верно
         assert collector.get_book_genre(name) == genre
 
-    def test_get_books_with_specific_genre_correct_list_book(self):
-        collector = BooksCollector()
-
-        # создаём переменные теста
-        test_name = 'Гордость и предубеждение и зомби'
-        test_genre = 'Ужасы'
-        name2 = 'Что делать, если ваш кот хочет вас убить'
-        genre2 = 'Детективы'
-
-
-        # добавляем две книги
-        collector.add_new_book(test_name)
-        collector.set_book_genre(test_name, test_genre)
-        collector.add_new_book(name2)
-        collector.set_book_genre(name2, genre2)
-
-
-        # проверяем, что по тестовому жанру получен список из одной тестовой книги
-        assert collector.get_books_with_specific_genre(test_genre) == [test_name]
-
-    def test_get_books_genre_correct_books_genre(self):
-        collector = BooksCollector()
-
+    def test_get_books_with_specific_genre_correct_list_book(self,collector):
         # создаём переменные теста
         name = 'Гордость и предубеждение и зомби'
         genre = 'Ужасы'
-
         # добавляем книгу
         collector.add_new_book(name)
         collector.set_book_genre(name, genre)
+        # проверяем, что по тестовому жанру получен список из одной тестовой книги
+        assert collector.get_books_with_specific_genre(genre) == [name]
 
+    def test_get_books_genre_correct_books_genre(self,collector):
+        # создаём переменные теста
+        name = 'Гордость и предубеждение и зомби'
+        genre = 'Ужасы'
+        # добавляем книгу
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
         # проверяем, что получаемый словарь books_genre такойже как тестовый
         assert collector.get_books_genre() == {name : genre}
 
-    def test_get_books_for_children_not_age_rating(self):
-        collector = BooksCollector()
-
-        # создаём переменные теста
-        test_name = 'Гордость и предубеждение и зомби'
-        test_genre = 'Ужасы'
-        name2 = 'Что делать, если ваш кот хочет вас убить'
-        genre2 = 'Комедии'
-
-
-        # добавляем две книги
-        collector.add_new_book(test_name)
-        collector.set_book_genre(test_name, test_genre)
-        collector.add_new_book(name2)
-        collector.set_book_genre(name2, genre2)
-
-        # проверяем, что что тестовой книги с возрастным рейтингом отсутствует в списке книг для детей
-        assert test_name not in collector.get_books_for_children()
-
-    def test_add_book_in_favorites_book_in_favorites(self):
-        collector = BooksCollector()
-
+    def test_get_books_for_children_not_age_rating(self,collector):
         # создаём переменные теста
         name = 'Гордость и предубеждение и зомби'
+        genre = 'Ужасы'
+        # добавляем две книги
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        # проверяем, что что тестовой книги с возрастным рейтингом отсутствует в списке книг для детей
+        assert name not in collector.get_books_for_children()
 
-
+    def test_add_book_in_favorites_book_in_favorites(self,collector):
+        # создаём переменные теста
+        name = 'Гордость и предубеждение и зомби'
         # добавляем книгу
         collector.add_new_book(name)
-
         # добавляем книгу в избранное
         collector.add_book_in_favorites(name)
-
         # проверяем, что список избранных содержит тестовую книгу
         assert collector.favorites == [name]
 
-    def test_delete_book_from_favorites_favorites_empty(self):
-        collector = BooksCollector()
-
+    def test_delete_book_from_favorites_favorites_empty(self,collector):
         # создаём переменные теста
         name = 'Гордость и предубеждение и зомби'
-
-
         # добавляем книгу
         collector.add_new_book(name)
-
         # добавляем книгу в избранное
         collector.add_book_in_favorites(name)
-
         # удаляем книгу из избранного
         collector.delete_book_from_favorites(name)
-
         # проверяем, что список избранного пуст
         assert not collector.favorites
 
-    def test_get_list_of_favorites_books_correct_favorites(self):
-        collector = BooksCollector()
-
+    def test_get_list_of_favorites_books_correct_favorites(self,collector):
         # создаём переменные теста
         name = 'Гордость и предубеждение и зомби'
-
-
         # добавляем книгу
         collector.add_new_book(name)
-
         # добавляем книгу в избранное
         collector.add_book_in_favorites(name)
-
         # проверяем, что получаемый словарь favorites такойже как тестовый
         assert collector.get_list_of_favorites_books() == [name]
